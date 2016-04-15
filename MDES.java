614fcd757a194de065326f76ec8b08c9af3400b4
ilmu <hjorvari@gmail.com>
2016-04-15 23:10:14 +1000

    ...

--- MDES.java
+++ MDES.java
@@ -6,7 +6,7 @@ public class MDES {
 		static String b4[] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
 		static String b3[] = {"000", "001", "010", "011", "100", "101", "110", "111"};
 		static String b2[]  = {"00", "01", "10", "11"};
-		static Map sbox = new HashMap();
+		static Map<String, String> sbox = new HashMap<String, String>();
 
 	public static void main(String[] args) {
 		
