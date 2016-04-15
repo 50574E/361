5fb9b0b2da197e8f1028d1e7829ec52ede5e8390
ilmu <hjorvari@gmail.com>
2016-04-15 23:04:48 +1000

    ...

--- MDES.java
+++ MDES.java
@@ -1,9 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LDES {
 
 		static String b4[] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
 		static String b3[] = {"000", "001", "010", "011", "100", "101", "110", "111"};
 		static String b2[]  = {"00", "01", "10", "11"};
-		static Map<String, String> sbox = new HashMap();
+		static Map sbox = new HashMap();
 
 	public static void main(String[] args) {
 		
